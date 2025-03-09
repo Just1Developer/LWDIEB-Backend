34324be1a91f74b874aae2de562cb1df8492ddb0
Just1Developer <heliosseries@gmail.com>
2025-03-09 15:57:00 +0100

    Project Setup

---
+++ src/main/java/net/justonedev/lwdiebbackend/LwdiebBackendApplication.java
@@ -0,0 +1,13 @@
+package net.justonedev.lwdiebbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LwdiebBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LwdiebBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/justonedev/lwdiebbackend/LwdiebBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.justonedev.lwdiebbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LwdiebBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
